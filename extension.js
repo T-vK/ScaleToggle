71090ec6c2a9f8f9e600486c1ecd9a11978b7d21
T-vK <T-vK@users.noreply.github.com>
2022-01-31 19:07:00 +0100

    Add source code

---
+++ extension.js
@@ -0,0 +1,77 @@
+// Simple Toogle Button Extension
+
+const TOGGLE_SCRIPT_PATH='.local/share/gnome-shell/extensions/scaletoggle@t-vk.github.com/scaletoggle.py' // relative to home
+
+const { Clutter, GObject, St, Gio } = imports.gi;
+const Main = imports.ui.main;
+const PanelMenu = imports.ui.panelMenu;
+
+const TOGGLE_ON_ICON = 'computer-symbolic';
+const TOGGLE_OFF_ICON = 'video-display-symbolic';
+
+const Indicator = GObject.registerClass(
+class Indicator extends PanelMenu.Button {
+    _init () {
+        super._init(0.0, 'Scale Toggle Button');
+    
+        this._icon = new St.Icon({
+            icon_name: 'video-display-symbolic',
+            style_class: 'system-status-icon',
+        });
+
+        this.add_child(this._icon);
+
+        this.connect('event', this._onClicked.bind(this));
+    }
+    _onClicked(actor, event) {
+        if ((event.type() !== Clutter.EventType.TOUCH_BEGIN && event.type() !== Clutter.EventType.BUTTON_PRESS)) {
+            // Some other non-clicky event happened; bail
+            return Clutter.EVENT_PROPAGATE;
+        }
+    
+        // Hopefully, I can figure out how to do this with imports.gi.Gio.DBus/DBusProxy in the future or even better without dbus entirely.
+        let proc = Gio.Subprocess.new(['python3', TOGGLE_SCRIPT_PATH], Gio.SubprocessFlags.NONE);
+        proc.wait_check_async(null, (proc, result) => {
+            try {
+                if (proc.wait_check_finish(result)) {
+                    log(`Successfully ran ${TOGGLE_SCRIPT_PATH}`);
+                } else {
+                    log(`Failed running ${TOGGLE_SCRIPT_PATH}`);
+                }
+            } catch (e) {
+                logError(e);
+            }
+        });
+
+        if (this._icon.icon_name === TOGGLE_ON_ICON) {
+            this._icon.icon_name = TOGGLE_OFF_ICON;
+            log('Toggle has been turned off!');
+        } else {
+            this._icon.icon_name = TOGGLE_ON_ICON;
+            log('Toggle has been turned on!');
+        }
+        
+        return Clutter.EVENT_PROPAGATE;
+    }
+});
+
+class Extension {
+    constructor(uuid) {
+        this._uuid = uuid;
+    }
+
+    enable() {
+        this._indicator = new Indicator();
+        
+        Main.panel.addToStatusArea(this._uuid, this._indicator);
+    }
+
+    disable() {
+        this._indicator.destroy();
+        this._indicator = null;
+    }
+}
+
+function init(meta) {
+    return new Extension(meta.uuid);
+}
---
+++ scaletoggle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+# https://dbus.freedesktop.org/doc/dbus-python/tutorial.html
+# https://github.com/GNOME/mutter/blob/b5f99bd12ebc483e682e39c8126a1b51772bc67d/data/dbus-interfaces/org.gnome.Mutter.DisplayConfig.xml
+# https://ask.fedoraproject.org/t/change-scaling-resolution-of-primary-monitor-from-bash-terminal/19892
+
+import dbus
+bus = dbus.SessionBus()
+
+display_config_well_known_name = "org.gnome.Mutter.DisplayConfig"
+display_config_object_path = "/org/gnome/Mutter/DisplayConfig"
+
+display_config_proxy = bus.get_object(display_config_well_known_name, display_config_object_path)
+display_config_interface = dbus.Interface(display_config_proxy, dbus_interface=display_config_well_known_name)
+
+serial, physical_monitors, logical_monitors, properties = display_config_interface.GetCurrentState()
+
+updated_logical_monitors=[]
+for x, y, scale, transform, primary, linked_monitors_info, props in logical_monitors:
+    if primary == 1:
+        scale = 2.0 if (scale == 1.0) else 1.0 # toggle scaling between 1.0 and 2.0 for the primary monitor
+    physical_monitors_config = []
+    for linked_monitor_connector, linked_monitor_vendor, linked_monitor_product, linked_monitor_serial in linked_monitors_info:
+        for monitor_info, monitor_modes, monitor_properties in physical_monitors:
+            monitor_connector, monitor_vendor, monitor_product, monitor_serial = monitor_info
+            if linked_monitor_connector == monitor_connector:
+                for mode_id, mode_width, mode_height, mode_refresh, mode_preferred_scale, mode_supported_scales, mode_properties in monitor_modes:
+                    if mode_properties.get("is-current", False): # ( mode_properties provides is-current, is-preferred, is-interlaced, and more)
+                        physical_monitors_config.append(dbus.Struct([monitor_connector, mode_id, {}]))
+                        if scale not in mode_supported_scales:
+                            print("Error: " + monitor_properties.get("display-name") + " doesn't support that scaling value! (" + str(scale) + ")")
+                        else:
+                            print("Setting scaling of: " + monitor_properties.get("display-name") + " to " + str(scale) + "!")
+    updated_logical_monitor_struct = dbus.Struct([dbus.Int32(x), dbus.Int32(y), dbus.Double(scale), dbus.UInt32(transform), dbus.Boolean(primary), physical_monitors_config])
+    updated_logical_monitors.append(updated_logical_monitor_struct)
+
+properties_to_apply = { "layout_mode": properties.get("layout-mode")}
+method = 1 # 2 means show a prompt before applying settings; 1 means instantly apply settings without prompt
+display_config_interface.ApplyMonitorsConfig(dbus.UInt32(serial), dbus.UInt32(method), updated_logical_monitors, properties_to_apply)
